50e58a4da4e777b3769d474531469dae02b92095
jagritx <jagritkamra1@gmail.com>
2023-03-09 12:56:28 +0530

    Changes

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ import videoRoutes from "./routes/videos.js";
 import commentRoutes from "./routes/comments.js";
 import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
-import cors from "cors";
+
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(cors());
+
 dotenv.config();
 mongoose.set("strictQuery", true);
 
